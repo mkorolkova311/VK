17e84ef64187dd4c8ace949a1dc691189d7c139b
Margarita Korolkova <mkorolkova311@gmail.com>
2020-12-23 21:18:29 +0300

    Likes

---
+++ src/main/java/domain/Likes.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class Likes {
+    public int count;
+    public boolean userLikes;
+    public boolean canLike;
+    public boolean canPublish;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isUserLikes() {
+        return userLikes;
+    }
+
+    public void setUserLikes(boolean userLikes) {
+        this.userLikes = userLikes;
+    }
+
+    public boolean isCanLike() {
+        return canLike;
+    }
+
+    public void setCanLike(boolean canLike) {
+        this.canLike = canLike;
+    }
+
+    public boolean isCanPublish() {
+        return canPublish;
+    }
+
+    public void setCanPublish(boolean canPublish) {
+        this.canPublish = canPublish;
+    }
+}
