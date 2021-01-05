98517f3de68ec63d81b8efdc288e3b9d3d1dd353
Margarita Korolkova <mkorolkova311@gmail.com>
2021-01-05 19:17:48 +0300

    Get/set

--- src/main/java/domain/Place.java
+++ src/main/java/domain/Place.java
@@ -10,4 +10,76 @@ public class Place {
     int country;
     int city;
     int address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public int getCheckins() {
+        return checkins;
+    }
+
+    public void setCheckins(int checkins) {
+        this.checkins = checkins;
+    }
+
+    public int getCountry() {
+        return country;
+    }
+
+    public void setCountry(int country) {
+        this.country = country;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setAddress(int address) {
+        this.address = address;
+    }
 }
